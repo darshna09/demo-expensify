600a5178a9b32bf9435d8f92b0ab95c0b6c2c684
darekha <darekha@cisco.com>
2020-07-19 16:59:09 +0530

    Getting started with Enzyme. Configuration with Jest.
    Setting up jest config and setUp files. Header UT updates

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
+// Using Enzyme
 test('should render header component', () => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+// Using react-test-renderer
+xtest('should render header component', () => {
     const renderer = new ShallowRenderer();
     renderer.render(<Header />);
     expect(renderer.getRenderOutput()).toMatchSnapshot();
