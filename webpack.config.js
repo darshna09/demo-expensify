5a425ff1a9bf80907f3cd7244ed90e187c0a1d3c
darekha <41057192+darshna09@users.noreply.github.com>
2021-08-03 10:54:35 +0530

    Package updates

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         }]
     },
     mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'eval-cheap-module-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
